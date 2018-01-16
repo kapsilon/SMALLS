edef74f858262efa56f5557995a37119fe7d3d6c
kapsilon <kapsilon47@gmail.com>
2018-01-16 04:25:48 +0300

    Powershell Hash of file to its name
    My variant (almost working) + working variant))
    Without file extension(

---
+++ HashToName.ps1
@@ -0,0 +1,41 @@
+﻿# Rename-Item Get-ChildItem ((Get-FileHash -Path (Get-ChildItem) -Algorithm SHA512).Hash)
+
+# (Get-FileHash -Path (Get-ChildItem) -Algorithm SHA512).Hash
+
+#Rename-Item -Path (Get-ChildItem.Name)   -NewName 123 
+
+#Get-ChildItem | Rename-Item -NewName 123
+
+#(Out-String -InputObject (Get-FileHash -Path (Get-ChildItem) -Algorithm SHA512).Hash)
+
+#Get-ChildItem | Rename-Item -NewName {Out-String -InputObject (Get-FileHash -Path (Get-ChildItem) -Algorithm SHA384).Hash)}
+
+#Rename-Item -Path "Batman (2016-) 004-011.jpg" -NewName (Get-FileHash -Path ("Batman (2016-) 004-011.jpg") -Algorithm SHA512).Hash
+
+#(Get-ChildItem).Name
+
+
+function Rename-FilesToSHA {
+    [CmdletBinding()]
+    Param(
+    [ValidateScript({ Test-Path $_ })]
+    $Path
+    )
+
+    $hasher = [System.Security.Cryptography.HashAlgorithm]::Create('SHA384')
+    $files = Get-ChildItem $Path -Recurse | Where-Object {-not $_.PSIsContainer}
+
+    foreach ($file in $files) {
+
+    $fs = New-Object IO.FileStream($file.FullName, 'Open')
+    $sb = New-Object System.Text.StringBuilder
+
+    $hasher.ComputeHash($fs) | ForEach-Object { [void]$sb.Append($_.ToString("x2")) }
+    $fs.Close()
+  
+    $file | Rename-Item -NewName $sb.ToString()
+    }
+}
+
+cd K:\cLIB\Hashing
+Rename-FilesToSHA -Path ./
