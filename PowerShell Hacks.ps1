8d3067814eb8e4dd1af7973675a643f6635933ca
kapsilon <kapsilon47@gmail.com>
2018-03-26 20:01:01 +0300

    mkdirs by ndirs

--- PowerShell Hacks.ps1
+++ PowerShell Hacks.ps1
@@ -1,2 +1,6 @@
 #All filenames in current directory without extension
 (Get-ChildItem).BaseName
+
+#New directories by names from local ndirs.txt
+#Get-Content .\ndirs.txt | ForEach {mkdir $_}
+New-Item -Path (Get-Content .\ndirs.txt) -ItemType directory
