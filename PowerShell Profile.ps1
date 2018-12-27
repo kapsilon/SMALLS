6180c7f6ed69fcdcfd23236c81109ec5b95b85c5
kapsilon <kapsilon47@gmail.com>
2018-12-27 06:27:29 +0300

    PowerShell No Custom PS ReadLine

--- PowerShell Profile.ps1
+++ PowerShell Profile.ps1
@@ -44,42 +44,6 @@ $Host.PrivateData.DebugBackgroundColor = 'DarkGray'
 $Host.PrivateData.VerboseBackgroundColor = 'DarkGray'
 $Host.PrivateData.ProgressBackgroundColor = 'Cyan'
 
-# PSReadline
-if (Get-Module -ListAvailable -Name "PSReadline") {
-    $options = Get-PSReadlineOption
-    $options.CommandForegroundColor = 'Yellow'
-    $options.ContinuationPromptForegroundColor = 'DarkBlue'
-    $options.DefaultTokenForegroundColor = 'DarkBlue'
-    $options.EmphasisForegroundColor = 'Cyan'
-    $options.ErrorForegroundColor = 'Red'
-    $options.KeywordForegroundColor = 'Green'
-    $options.MemberForegroundColor = 'DarkCyan'
-    $options.NumberForegroundColor = 'DarkCyan'
-    $options.OperatorForegroundColor = 'DarkGreen'
-    $options.ParameterForegroundColor = 'DarkGreen'
-    $options.StringForegroundColor = 'Blue'
-    $options.TypeForegroundColor = 'DarkYellow'
-    $options.VariableForegroundColor = 'Green'
-    $options.CommandBackgroundColor = 'Black'
-    $options.ContinuationPromptBackgroundColor = 'Black'
-    $options.DefaultTokenBackgroundColor = 'Black'
-    $options.EmphasisBackgroundColor = 'Black'
-    $options.ErrorBackgroundColor = 'Black'
-    $options.KeywordBackgroundColor = 'Black'
-    $options.MemberBackgroundColor = 'Black'
-    $options.NumberBackgroundColor = 'Black'
-    $options.OperatorBackgroundColor = 'Black'
-    $options.ParameterBackgroundColor = 'Black'
-    $options.StringBackgroundColor = 'Black'
-    $options.TypeBackgroundColor = 'Black'
-    $options.VariableBackgroundColor = 'Black'
-}
-if ($host.Name -eq 'ConsoleHost')
-{
-    Import-Module PSReadline
-    Set-PSReadlineKeyHandler -Key Ctrl+Tab -Function MenuComplete
-}
-
 # All Clear
 clear
 cd\
