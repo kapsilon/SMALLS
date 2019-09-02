879517fc9150e38f028b6e7957907c9430debf70
kapsilon <kapsilon47@gmail.com>
2019-09-02 17:46:29 +0300

    PS Module RoboCopy

--- Windows/PowerShell Profile.ps1
+++ Windows/PowerShell Profile.ps1
@@ -2,30 +2,6 @@
 #Copy-Item "PowerShell Profile.ps1" -Destination "${home}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
 
-# Copying all folders and files
-Function RoboCopy-All-Files($oldDirectory, $newDirectory) {
-    robocopy  `
-        $oldDirectory `
-        $newDirectory `
-        /E `
-        /COPY:DAT `
-        /DCOPY:DAT `
-        /R:100 `
-        /sl
-}
-Set-Alias copyall -Value RoboCopy-All-Files
-
-# Create tree of files with zero length
-Function RoboCopy-Zero-Tree($oldDirectory, $newDirectory) {
-    robocopy  `
-        $oldDirectory `
-        $newDirectory `
-        /CREATE `
-        /MIR `
-        /COPY:DAT `
-        /DCOPY:DAT `
-        /R:100
-}
 
 
 # All filenames in current directory without extension
@@ -56,18 +32,6 @@ Function Find-NotUniqueStrings-InFile($file) {
     else { echo OK }
 }
 Set-Alias uniqs -Value Find-NotUniqueStrings-InFile
-
-# Very simple diff of file trees
-Function Compare-Difference-Of-Trees($old, $new, $diff) {
-    if ($old -and $new -and $diff) {
-        $treeold = tree $old
-        $treenew = tree $new
-        Compare-Object $treeold $treenew > $diff
-    } else{
-        echo "difftree path/to/dir1 path/to/dir2 path/to/difffile"
-    }
-}
-Set-Alias difftree -Value Compare-Difference-Of-Trees
 
 # Get full path of symbolic link
 Function Get-Full-Path-of-Symbol-Link($path) {
---
+++ Windows/RoboCopy.psm1
@@ -0,0 +1,36 @@
+# Copying all folders and files
+Function RoboCopy-All-Files($oldDirectory, $newDirectory) {
+    robocopy  `
+        $oldDirectory `
+        $newDirectory `
+        /E `
+        /COPY:DAT `
+        /DCOPY:DAT `
+        /R:100 `
+        /sl
+}
+Set-Alias copyall -Value RoboCopy-All-Files
+
+# Create tree of files with zero length
+Function RoboCopy-Zero-Tree($oldDirectory, $newDirectory) {
+    robocopy  `
+        $oldDirectory `
+        $newDirectory `
+        /CREATE `
+        /MIR `
+        /COPY:DAT `
+        /DCOPY:DAT `
+        /R:100
+}
+
+# Very simple diff of file trees
+Function Compare-Difference-Of-Trees($old, $new, $diff) {
+    if ($old -and $new -and $diff) {
+        $treeold = tree $old
+        $treenew = tree $new
+        Compare-Object $treeold $treenew > $diff
+    } else{
+        echo "difftree path/to/dir1 path/to/dir2 path/to/difffile"
+    }
+}
+Set-Alias difftree -Value Compare-Difference-Of-Trees
