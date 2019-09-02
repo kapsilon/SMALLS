cc156be24198586ffb8be25fc490390633dcfbfe
kapsilon <kapsilon47@gmail.com>
2019-09-02 17:43:15 +0300

    PS Module YouTube

--- Windows/PowerShell Profile.ps1
+++ Windows/PowerShell Profile.ps1
@@ -27,48 +27,6 @@ Function RoboCopy-Zero-Tree($oldDirectory, $newDirectory) {
         /R:100
 }
 
-# Dowload video from YouTube or other video-hostings with youtube-dl+ffmpeg
-Function Dowload-Video-From-YouTube ($link, $format) {
-    #Set YouTube-DL
-    $ytdlFolder = "C:\Program Files\YouTube-DL\"
-    $ytdlExe = "youtube-dl.exe"
-    $ytdl = $ytdlFolder + $ytdlExe
-    #Set dowlnload
-    $downloadFolder = "G:\Download\"
-    $downloadFile = "%(title)s__%(resolution)s.%(ext)s"
-    $dl = $downloadFolder + $downloadFile
- 
-    $host.UI.RawUI.WindowTitle = "YouTube-DL"
-    if ($link -and $format) {
-        if ($format -eq "1080") {
-            $format = "-f 137+22"
-        }
-        elseif ($format -eq "subs") {
-            $format = "--write-sub --convert-subs srt --sub-lang en,ru --skip-download"
-        }
-        elseif ($format -eq "asubs") {
-            $format = "--write-auto-sub --sub-lang en,ru --skip-download"
-        }
-        else {
-            $format = "-f " + $format
-        }
-        $arguments = $format + " -o " + $dl + " " + $link
-        Start-Process -FilePath $ytdl -ArgumentList $arguments -NoNewWindow -Wait
-    }
-    elseif ($link) {
-        $arguments = "-F " + $link
-        Start-Process -FilePath $ytdl -ArgumentList $arguments -NoNewWindow -Wait
-    }
-    else {
-        echo "For view all variants: 'youtube https://yourlink.to/video'"
-        echo "For download variant (video+audio): 'youtube https://yourlink.to/video 137+22'"
-        echo "For download 1080p+bestaudio: 'youtube https://yourlink.to/video 1080'"
-        echo "For download subtitles: 'youtube https://yourlink.to/video subs'"
-        echo "For download automated subtitles: 'youtube https://yourlink.to/video subs'"
-    }
-    $host.UI.RawUI.WindowTitle = "PowerShell"
-}
-Set-Alias youtube -Value Dowload-Video-From-YouTube
 
 # All filenames in current directory without extension
 Function Basenames-OfFiles-InCurrentDirectory {
