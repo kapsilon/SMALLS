70e7bb5b83a787022945f445541c58c261c1b740
kapsilon <kapsilon47@gmail.com>
2020-04-19 00:09:06 +0300

    Rambox Injections VK Disable header bar

--- Rambox Injections.js
+++ Rambox Injections.js
@@ -1,5 +1,8 @@
 //VK: Disable social junk
 document.getElementById('side_bar').style.display = "none";
+//VK: Disable header bar
+document.getElementById('page_header_cont').style.display = "none";
+document.getElementById('page_body').style.marginTop = "0";
 
 //WhatsApp: Enter <=> Ctrl+Enter
 document.addEventListener('keydown', function(e){
