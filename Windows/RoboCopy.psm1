01d352f810c29a2812d4f339ec1acbd5c32d9c62
kapsilon <kapsilon47@gmail.com>
2020-05-18 07:40:00 +0300

    RoboCopy Some Refactoring

--- Windows/RoboCopy.psm1
+++ Windows/RoboCopy.psm1
@@ -36,10 +36,10 @@ Function RoboCopy-Zero-Tree($oldDirectory, $newDirectory) {
 Set-Alias copytree -Value RoboCopy-Zero-Tree
 
 # Very simple diff of file trees
-Function Compare-Difference-Of-Trees($old, $new, $diff) {
-    if ($old -and $new -and $diff) {
-        $treeold = tree $old
-        $treenew = tree $new
+Function Compare-Difference-Of-Trees($oldDirectory, $newDirectory, $diff) {
+    if ($oldDirectory -and $new -and $diff) {
+        $treeold = tree $oldDirectory
+        $treenew = tree $newDirectory
         Compare-Object $treeold $treenew > $diff
     } else{
         Write-Output "difftree path/to/dir1 path/to/dir2 path/to/difffile"
