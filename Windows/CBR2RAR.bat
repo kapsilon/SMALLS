dc61fa5dd2914e7a88265cab738f9d580985469f
kapsilon <kapsilon47@gmail.com>
2020-12-19 00:25:16 +0300

    Update CBR2RAR
    A or An?

--- Windows/CBR2RAR.bat
+++ Windows/CBR2RAR.bat
@@ -5,6 +5,10 @@ ren *.cbr *.rar
 ren *.cbz *.rar
 ren *.zip *.rar
 
-::Delete article THE
+::Delete article The/A/An
 ren "The *.rar" "////*.rar"
 ren "The_*.rar" "////*.rar"
+ren "A *.rar" "//*.rar"
+ren "A_*.rar" "//*.rar"
+ren "An *.rar" "///*.rar"
+ren "An_*.rar" "///*.rar"
